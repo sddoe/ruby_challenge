33f36b4d47c9e8d2c0b70fc3b579a196b4205313
Sergio David Perdomo Rivera <sergio.931129@gmail.com>
2022-10-08 03:30:47 -0500

    Add exemption items and constants

---
+++ lib/constants.rb
@@ -0,0 +1,8 @@
+#exemption items and tax rates constants
+
+module Constants
+  EXEMPTION_ITEMS = './lib/exemption_items.yml'
+  BASIC_SALES_TAX = 0.10
+  IMPORTED_GOOD_TAX = 0.05
+  ROUND_UP_FACTOR = 0.05
+end
